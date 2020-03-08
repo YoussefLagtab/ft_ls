c5779186e84b6468c9f629a8b3e43c50746149aa
Mohamed El idrissi el wali <mel-idri@e2r4p14.1337.ma>
2020-03-08 15:53:16 +0100

    fix compare_mtime order

--- get_cmp_func/compare_mtime.c
+++ get_cmp_func/compare_mtime.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   compare_mtime.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 19:10:22 by ylagtab           #+#    #+#             */
-/*   Updated: 2020/02/19 19:30:48 by ylagtab          ###   ########.fr       */
+/*   Updated: 2020/03/08 15:50:37 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int		compare_mtime(t_list *node1, t_list *node2)
 	inode2 = (t_inode*)node2->content;
 	inode1_mtime = inode1->st.st_mtimespec.tv_sec;
 	inode2_mtime = inode2->st.st_mtimespec.tv_sec;
-	if (inode1_mtime < inode2_mtime)
-		return (-1);
 	if (inode1_mtime > inode2_mtime)
+		return (-1);
+	if (inode1_mtime < inode2_mtime)
 		return (1);
 	return (ft_strcmp(inode1->name, inode2->name));
 }
