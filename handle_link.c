d8976b3bd0f3071c5f0e7f6ab7f65fe325fb4caa
Mohamed El idrissi el wali <mel-idri@e2r4p14.1337.ma>
2020-03-13 19:20:13 +0100

    replace is_symlink_to_dir() with a variable
    replace is_symlink_to_dir() with a variable condition in handle_link()

--- handle_link.c
+++ handle_link.c
@@ -3,32 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   handle_link.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 16:37:23 by ylagtab           #+#    #+#             */
-/*   Updated: 2020/02/19 15:00:50 by ylagtab          ###   ########.fr       */
+/*   Updated: 2020/03/13 01:34:43 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static int	is_symlink_to_dir(char *filename)
-{
-	struct stat st;
-
-	return (stat(filename, &st) == 0 && S_ISDIR(st.st_mode));
-}
-
 void		handle_link(t_inode *inode, t_args *ls_args)
 {
-	int end_with_slash;
+	int				end_with_slash;
+	int				is_symlink_to_dir;
+	struct  stat	st;
 
 	end_with_slash = inode->name[ft_strlen(inode->name) - 1] == '/';
-	if (is_symlink_to_dir(inode->name))
+	is_symlink_to_dir = stat(inode->name, &st) == 0 && S_ISDIR(st.st_mode);
+	if (is_symlink_to_dir)
 	{
 		if (!F_IS_SET(g_options, OPT_L) || end_with_slash)
+		{
 			ft_enqueue(&(ls_args->dirs), &inode, sizeof(t_inode));
-		return ;
+			return ;
+		}
 	}
 	ft_enqueue(&(ls_args->files), &inode, sizeof(t_inode));
 }
