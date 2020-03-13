d4e35506405b8da69a9fc70f5b93c9b396d0f5dc
Mohamed El idrissi el wali <mel-idri@e2r4p14.1337.ma>
2020-03-13 21:09:56 +0100

    fix ACL to not follow links

--- print_inodes_longlist/fill_acl_ext.c
+++ print_inodes_longlist/fill_acl_ext.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fill_acl_ext.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 09:44:23 by ylagtab           #+#    #+#             */
-/*   Updated: 2020/02/20 01:00:27 by ylagtab          ###   ########.fr       */
+/*   Updated: 2020/03/13 04:34:01 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	fill_acl_ext(t_inode_details *i_details, t_inode *inode)
 	char	*full_path;
 
 	full_path = ft_strglue(inode->path, '/', inode->name);
-	acl = acl_get_file(full_path, ACL_TYPE_EXTENDED);
+	acl = acl_get_link_np(full_path, ACL_TYPE_EXTENDED);
 	xattr = listxattr(full_path, NULL, 0, XATTR_NOFOLLOW);
 	i_details->acl_ext = ' ';
 	if (xattr > 0)
